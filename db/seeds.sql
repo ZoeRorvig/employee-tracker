3aed2da19c4a30dec14238f5d3ef6001b63c315b
Zoe Rorvig <zrorvig@gmail.com>
2023-01-07 12:32:52 -0500

    set up employees table

--- db/seeds.sql
+++ db/seeds.sql
@@ -26,6 +26,8 @@
         -- Zoe Rorvig --
     -- Quality Assurance Engineer --
         -- Scott Westerfeld --
+    -- Quality Manager --
+        -- Jim Toeger --
 -- Supply Chain Management --
     -- Shipping Manager --
         -- Kimberly Jones --
@@ -58,9 +60,28 @@ INSERT INTO role (title, salary, department_id)
     ('Quality Technician', 100000, 4),
     ('Quality System Engineer', 100000, 4),
     ('Quality Assurance Engineer', 100000, 4),
+    ('Quality Manager', 100000, 4),
     ('Shipping Manager', 100000, 5),
     ('SReceiving Manager', 100000, 5),
     ('Shipping Associate', 100000, 5),
     ('Receiving Associate', 100000, 5);
 
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES
+    ('Juliet', 'Sparks', 1, 4),
+    ('Hans-Augusto', 'Rey', 2, 4),
+    ('Enzo', 'Blevins', 3, 4),
+    ('Brandon', 'Sanderson', 4, null),
+    ('Claire', 'Case', 5, null),
+    ('Elsa', 'Knight', 6, 5),
+    ('Amorina', 'Brucca', 7, 5),
+    ('Margret', 'Ray', 8, null),
+    ('Anita', 'Brewer', 9, 8),
+    ('Tamsyn', 'Muir', 10, 13),
+    ('Zoe', 'Rorvig', 11, 13),
+    ('Scott', 'Westerfeld', 12, 13),
+    ('Jim', 'Troeger', 13, null),
+    ('Kimberly', 'Jones', 14, null),
+    ('Kelvin', 'Slater', 15, null),
+    ('Samantha', 'Shannon', 16, 14),
+    ('Marco', 'Allison', 17, 15);
